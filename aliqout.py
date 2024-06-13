3cc49828f6d083129fc8cad405defb97d61dbc9e
David Ingraham <davidingrahamf@gmail.com>
2024-06-13 15:46:57 -0400

    aliqout sequence finder working

--- aliqout.py
+++ aliqout.py
@@ -5,6 +5,7 @@
 #need to keep a dictionary of each number in the sequence and its corespogning value as the number of times wehave seen it. if that key exceeds 1 then we have a loop
 #evetually p will plot it
 from math import floor
+import sys
 
 
 def getDivis(num: int) -> 'list[int]':
@@ -25,31 +26,46 @@ def getDivis(num: int) -> 'list[int]':
         return divis
     
     else:
-        return f'{num} is prime \n --- \n'
+        print(f'{num} is prime')
+        return divis
     
-
+loop_tracker ={int: int}
 def aliqout(num: int):
-    original_num = num
-    loop_tracker ={int: int}
+    original_num: int = num
+    divis: list[int] = getDivis(num)
+    next_num: int = sum(divis)
 
-
-
-    divis = getDivis(num)
-    next_num = sum(divis)
-
-    if next_num in loop_tracker:
+    if next_num in loop_tracker.keys():
         loop_tracker[next_num] +=1
 
     else:
         loop_tracker[next_num] =1
 
+    #loop_tracker.pop('<class \'int\'>')        
+    print(divis)
     print(loop_tracker)
+ 
+
+    """"""
+    
+    values: list[int] = []
+
+    for val in loop_tracker.values():
+        values.append(val)
+        #print(f'values list: {values}')
+
+    for val in values:
+        #print(type(val))
+        #print(val)
+
+        if type(val) ==int:
+            if val > 1:
+                return f'\n{sys.argv[1]} entered a loop after {len(loop_tracker)-2} itterations'
+        else: 
+            continue
+
 
     res = aliqout(next_num)
-
-    for val in loop_tracker.values:
-        if val > 1:
-            return f'{original_num} entered a loop'
 
     return res
 
@@ -64,12 +80,15 @@ def main():
     #for i in range(30):
      #   print(getDivis(i))
 
-    print(aliqout(6))
+    print(aliqout(int(sys.argv[1])))
+    
     #print(getDivis(6))
      
     #print([type(item) for item in getDivis(6)])
 
-     #print(getDivis(100))
+    #print(getDivis(12161))
+
+    #print(getDivis(3))
 
      
 
